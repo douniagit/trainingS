12765677a67c09203104f2a80a30f260243db3c8
dounia <d.fergane@gmail.com>
2017-03-01 23:52:20 +0100

    add css to Activite component

--- src/Activite/Activite.jsx
+++ src/Activite/Activite.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import contenu from '../Card/data.js';
 import Navbar from "../navbar/Navbar.jsx";
 import Footer from "../footer/Footer.jsx";
-
+import "./activite.css";
 
 export default class Activite extends React.Component {
 
 	render(){
 		return(
-		<div className="Activite">
+		<div className="activite">
 		<Navbar />
-			<div className="Presentation">
+			<div className="presentation">
 				<h2>PRESENTATION DES DIFFERENTES ACTIVITES PROPOSEES</h2>
 				
               { contenu.activites.map(menu =>
               	<div className="prezactiv">
 	                <div className="titreactiv">{menu.name}</div>
-	                  <img src={menu.image} alt="" className="img"/>
+	                  <img src={menu.image} alt="" className="img3"/>
 	                  <div className="textactiv">
 	                    <div className="prez">{menu.description}</div>
 					</div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {browserHistory} from 'react-router';
 import Routes from './Routes.js';
+import "./index.css";
 
 ReactDOM.render(
   <Routes history={browserHistory} />,
